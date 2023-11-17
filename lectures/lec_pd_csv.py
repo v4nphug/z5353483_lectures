9c429f0261132b6fb3eba922385c779c8cb656c4
Van Phung <87002566+v4nphug@users.noreply.github.com>
2023-11-17 15:38:53 +1100

    Update lec_pd_csv.py

--- lectures/lec_pd_csv.py
+++ lectures/lec_pd_csv.py
@@ -19,24 +19,24 @@ QAN_CLOSE_CSV = os.path.join(cfg.DATADIR, 'qan_close_ser.csv')
 
 # Load the data contained in qan_prc_2020.csv to a DF
 
-#qan_naive_read = pd.read_csv(QAN_PRC_CSV) 
-#print(qan_naive_read)
-#qan_naive_read.info()
+qan_naive_read = pd.read_csv(QAN_PRC_CSV) 
+print(qan_naive_read)
+qan_naive_read.info()
 #
 
 # Using the `set_index` method
 
-#qan_naive_read.set_index('Date', inplace=True) 
-#print(qan_naive_read)
-#qan_naive_read.info()
+qan_naive_read.set_index('Date', inplace=True) 
+print(qan_naive_read)
+qan_naive_read.info()
 #
 
 # Using the `index_col` parameter:
 
-#qan_better_read = pd.read_csv(QAN_PRC_CSV, index_col='Date') 
-#print(qan_better_read)
+qan_better_read = pd.read_csv(QAN_PRC_CSV, index_col='Date') 
+print(qan_better_read)
 #
-#qan_better_read.info()
+qan_better_read.info()
 #
 
 
@@ -45,11 +45,11 @@ QAN_CLOSE_CSV = os.path.join(cfg.DATADIR, 'qan_close_ser.csv')
 #   Storing data to a CSV file 
 # ----------------------------------------------------------------------------
 # First, we read the data into a dataframe
-#qan_better_read = pd.read_csv(QAN_PRC_CSV, index_col='Date')  
+qan_better_read = pd.read_csv(QAN_PRC_CSV, index_col='Date')  
 
 # We then save the data into the file located at QAN_NOHEAD_CSV above.
 # The column headers will not be saved
-#qan_better_read.to_csv(QAN_NOHEAD_CSV, header=False)  
+qan_better_read.to_csv(QAN_NOHEAD_CSV, header=False)  
 
 
 
@@ -58,31 +58,31 @@ QAN_CLOSE_CSV = os.path.join(cfg.DATADIR, 'qan_close_ser.csv')
 # ----------------------------------------------------------------------------
 # Create a series from a dataframe
 
-#qan_better_read = pd.read_csv(QAN_PRC_CSV, index_col='Date')
-#ser = qan_better_read.loc[:, 'Close']
-#print(ser)
-#ser.to_csv(QAN_CLOSE_CSV)
+qan_better_read = pd.read_csv(QAN_PRC_CSV, index_col='Date')
+ser = qan_better_read.loc[:, 'Close']
+print(ser)
+ser.to_csv(QAN_CLOSE_CSV)
 #
 
 
 # Note that the name of the series will be the same as the column label
-#print(ser.name) 
+print(ser.name) 
 
 # Create a series without a name
 
-#dates = list(qan_better_read.index)
-#data = list(qan_better_read.Close)
-#ser_no_name = pd.Series(data, index=dates)
-#print(ser_no_name)
-#print(f'The name of the series is {ser_no_name.name}')
+dates = list(qan_better_read.index)
+data = list(qan_better_read.Close)
+ser_no_name = pd.Series(data, index=dates)
+print(ser_no_name)
+print(f'The name of the series is {ser_no_name.name}')
 #
 ## Now save it to the same CSV file as above
-#ser_no_name.to_csv(QAN_CLOSE_CSV)
+ser_no_name.to_csv(QAN_CLOSE_CSV)
 #
 #
 ## Read the data back
-#as_df = pd.read_csv(QAN_CLOSE_CSV)
-#print(as_df)
+as_df = pd.read_csv(QAN_CLOSE_CSV)
+print(as_df)
 #
 
 
@@ -92,10 +92,10 @@ QAN_CLOSE_CSV = os.path.join(cfg.DATADIR, 'qan_close_ser.csv')
 # Using the ser_no_name created above
 # Save the contents without column headers
 
-#ser_no_name.to_csv(QAN_CLOSE_CSV, header=False)
+ser_no_name.to_csv(QAN_CLOSE_CSV, header=False)
 ## Read it back 
-#as_df = pd.read_csv(QAN_CLOSE_CSV, header=None, index_col=0)
-#print(as_df)
+as_df = pd.read_csv(QAN_CLOSE_CSV, header=None, index_col=0)
+print(as_df)
 #
 
 
@@ -105,10 +105,10 @@ QAN_CLOSE_CSV = os.path.join(cfg.DATADIR, 'qan_close_ser.csv')
 # Using the ser_no_name created above
 # Save the contents without column headers
 
-#ser_no_name.to_csv(QAN_CLOSE_CSV, header=False)
+ser_no_name.to_csv(QAN_CLOSE_CSV, header=False)
 ## Read it back 
-#as_df = pd.read_csv(QAN_CLOSE_CSV, header=None, names=["Date", "Close"], index_col=0)
-#print(as_df)
+as_df = pd.read_csv(QAN_CLOSE_CSV, header=None, names=["Date", "Close"], index_col=0)
+print(as_df)
 #
 
 
@@ -118,12 +118,12 @@ QAN_CLOSE_CSV = os.path.join(cfg.DATADIR, 'qan_close_ser.csv')
 # Using the ser_no_name created above
 # Save the contents without column headers
 
-#ser_no_name.to_csv(QAN_CLOSE_CSV, 
-#        index_label="Date", 
-#        header=['Close'],
-#        )
+ser_no_name.to_csv(QAN_CLOSE_CSV, 
+        index_label="Date", 
+       header=['Close'],
+        )
 ## Read it back 
-#as_df = pd.read_csv(QAN_CLOSE_CSV, index_col=0)
-#print(as_df)
+as_df = pd.read_csv(QAN_CLOSE_CSV, index_col=0)
+print(as_df)
 #
 
